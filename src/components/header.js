5373fae6b592f1728f22d0d57caaaf24474b8ff9
samclee <samclee@gmail.com>
2020-04-18 22:14:59 -0700

    changed header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,9 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -16,16 +14,8 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+      <h1 style={{ margin: 0, textAlign: "center" }}>
           {siteTitle}
-        </Link>
       </h1>
     </div>
   </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -33,12 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        {children}
       </div>
     </>
   )
